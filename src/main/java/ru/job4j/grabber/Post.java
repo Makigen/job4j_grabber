3ad6dce23653889fb9acf893dc60eabc5340e50d
Makigen <makige.ee@mail.ru>
2021-06-05 23:48:09 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,52 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private int id;
+    private String name;
+    private String text;
+    private String link;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    private LocalDateTime created;
+}
