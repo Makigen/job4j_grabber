73874da147caffbbf7c3af0b03b7f3e5c9ee0471
Makigen <makige.ee@mail.ru>
2021-06-08 18:06:16 +0300

    2.1. Преобразование даты [#289476 #165021]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -1,30 +1,40 @@
 package ru.job4j.grabber.utils;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
-    @Override
+    Map<String, Integer> months = new HashMap<>();
+    {
+        months.put("янв", 1);
+        months.put("фев", 2);
+        months.put("мар", 3);
+        months.put("апр", 4);
+        months.put("май", 5);
+        months.put("июн", 6);
+        months.put("июл", 7);
+        months.put("авг", 8);
+        months.put("сен", 9);
+        months.put("окт", 10);
+        months.put("ноя", 11);
+        months.put("дек", 12);
+    }
+
     public LocalDateTime parse(String parse) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MMM yy, HH:mm");
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd MMM yy,");
-        String time = parse.substring(parse.length() - 6);
-        if (parse.length() == 15) {
-            parse = "0" + parse;
-        }
+        LocalDate localdate;
+        String time = parse.substring(parse.length() - 5);
         if (parse.contains("вчера")) {
-            parse = df.format(LocalDateTime.now().minusDays(1)) + time;
+            localdate = LocalDate.now().minusDays(1);
+        } else if (parse.contains("сегодня")) {
+            localdate = LocalDate.now();
+        } else {
+            int year = Integer.parseInt("20" + parse.substring(parse.length() - 9, parse.length() - 7));
+            int month = months.get(parse.split(" ")[1].split(" ")[0]);
+            localdate = LocalDate.of(year, month, Integer.parseInt(parse.substring(0, 1)));
         }
-        if (parse.contains("сегодня")) {
-            parse = df.format(LocalDateTime.now()) + time;
-        }
-        if (parse.contains("май")) {
-            parse = parse.substring(0, 3) + "мая" + parse.substring(6);
-        } else if (parse.contains("фев")) {
-            parse = parse.substring(0, 6) + "р." + parse.substring(6);
-        } else if (!parse.contains(".")) {
-            parse = parse.substring(0, 6) + ". " + parse.substring(7);
-        }
-        return LocalDateTime.parse(parse, dtf);
+        return LocalDateTime.of(localdate, LocalTime.parse(time));
     }
 }
